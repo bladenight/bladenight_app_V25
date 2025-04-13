4a4c66e46e4e7bb7aedd557f57bcbb5268aa76db
larshuth <it@huth.app>
2025-04-13 11:37:42 +0200

    - fixed height issue with sponsor carousel

--- lib/generated/intl/messages_fr.dart
+++ lib/generated/intl/messages_fr.dart
@@ -1004,7 +1004,7 @@ class MessageLookup extends MessageLookupByLibrary {
       "Fonctions spéciales - ne changez que si vous savez ce que vous faites !",
     ),
     "speed": MessageLookupByLibrary.simpleMessage("vitesse"),
-    "sponsors": MessageLookupByLibrary.simpleMessage("Nos\nsponsors"),
+    "sponsors": MessageLookupByLibrary.simpleMessage("Nos sponsors"),
     "start": MessageLookupByLibrary.simpleMessage("Commence le"),
     "startLocationWithoutParticipating": MessageLookupByLibrary.simpleMessage(
       "Lieu de départ sans participation",
